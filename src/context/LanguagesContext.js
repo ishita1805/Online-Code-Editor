fcd8f55084f798fba74ecc8cf47bf55cdb8e4c4b
Ishita Kabra <ishitakabra1805@gmail.com>
2021-07-17 22:00:50 +0530

    complete

--- src/context/LanguagesContext.js
+++ src/context/LanguagesContext.js
@@ -15,8 +15,8 @@ const LangProvider = ({ children }) => {
 
 
    useEffect(() => {
-       let id = window.location.pathname;
-       id = id.replace('/Dyte-React-Task/','');
+       let id = window.location.href;
+       id = id.replace('https://ishita1805.github.io/Dyte-React-Task/','');
        if(id !== '') {
         axios.get('https://cors-remove.herokuapp.com/getraw', {
             params: {
